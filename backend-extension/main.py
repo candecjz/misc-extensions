a9052128f783084270b38646b000d74a9c1c73d9
Macarena Martín <macarenamartin1111@gmail.com>
2026-01-16 21:02:40 -0300

    The update date for each objective was added, along with an update indicator displaying a 45-day range. Icons and styles were also added.

--- backend-extension/main.py
+++ backend-extension/main.py
@@ -6,22 +6,13 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import requests
 from typing import Optional
-
+from datetime import datetime
 
 app = FastAPI(title="Punto de Extensión HSI")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 static_path = os.path.join(BASE_DIR, "static")
 templates_path = os.path.join(BASE_DIR, "templates")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app.mount("/static", StaticFiles(directory=static_path), name="static")
 templates = Jinja2Templates(directory=templates_path)
 
@@ -35,82 +26,85 @@ OBJETIVOS_DATA = [
         "id": 1,
         "name": "Identificación y accesibilidad de la población a cargo",
         "type": "flowchart",
-        "miniature1": "/static/images/1.png",
+        "fecha": "06/2025",
         "pdfDR": "/static/pdfs/1.pdf",
     },
     {
         "id": 2,
         "name": "Reducción de la morbimortalidad materno infantil",
         "type": "objective",
+        "fecha": "12/2025",
         "children": [
             {
                 "id": 2.1,
                 "name": "Persona gestante",
                 "type": "flowchart",
-                "miniature1": "/static/images/2-a.png",
+                "fecha": "09/2025",
                 "pdfDR": "/static/pdfs/2-A.pdf",
             },
             {
                 "id": 2.2,
-                "name": "Chagas - Transmisión vertical materno infantil",
+                "name": "Control de niños menores de 3 años",
                 "type": "flowchart",
-                "miniature1": "/static/images/2-b.png",
+                "fecha": "12/2025",
                 "pdfDR": "/static/pdfs/2-B.pdf",
             },
             {
                 "id": 2.3,
-                "name": "Control de niños menores de 3 años",
+                "name": "Chagas - Transmisión vertical materno infantil",
                 "type": "flowchart",
-                "miniature1": "/static/images/2-c.png",
+                "fecha": "11/2025",
                 "pdfDR": "/static/pdfs/2-C.pdf",
             },
-            {
-                "id": 2.4,
-                "name": "Control de niños menores de 10 años",
-                "type": "flowchart",
-                "miniature1": "/static/images/2-d.png",
-                "pdfDR": "/static/pdfs/2-D.pdf",
-            }
-        ],
+        ],  
     },
     {
         "id": 3,
         "name": "Salud de la niñez y adolescencia",
         "type": "objective",
+        "fecha": "12/2025",
         "children": [
             {
                 "id": 3.1,
                 "name": "Control escolar - Crecer sanos",
                 "type": "flowchart",
-                "miniature1": "/static/images/3-a.png",
+                "fecha": "10/2025",
                 "pdfDR": "/static/pdfs/3-A.pdf",
             },
             {
                 "id": 3.2,
-                "name": "Prevención y asistencia en adolescentes",
+                "name": "Control de niños menores de 10 años",
                 "type": "flowchart",
-                "miniature1": "/static/images/3-b.png",
+                "fecha": "12/2025",
                 "pdfDR": "/static/pdfs/3-B.pdf",
             },
-        ]
+            {
+                "id": 3.3,
+                "name": "Prevención y asistencia en adolescentes",
+                "type": "flowchart",
+                "fecha": "12/2025",
+                "pdfDR": "/static/pdfs/3-C.pdf",
+            },
+        ],
     },
     {
         "id": 4,
         "name": "Salud del adulto y adulto mayor",
         "type": "objective",
+        "fecha": "06/2025",
         "children": [
             {
                 "id": 4.1,
                 "name": "Atención a la persona adulta",
                 "type": "flowchart",
-                "miniature1": "/static/images/4-a.png",
+                "fecha": "06/2025",
                 "pdfDR": "/static/pdfs/4-A.pdf",
             },
             {
                 "id": 4.2,
                 "name": "Atención a la persona mayor en PNA",
                 "type": "flowchart",
-                "miniature1": "/static/images/4-b.png",
+                "fecha": "06/2025",
                 "pdfDR": "/static/pdfs/4-B.pdf",
             },
         ],
@@ -119,31 +113,33 @@ OBJETIVOS_DATA = [
         "id": 5,
         "name": "Prevención y atención de ECNT",
         "type": "objective",
+        "fecha": "01/2026",
         "children": [
             {
                 "id": 5.1,
                 "name": "Atención de HTA en adultos",
                 "type": "flowchart",
-                "miniature1": "/static/images/5-a.png",
+                "fecha": "09/2025",
                 "pdfDR": "/static/pdfs/5-A.pdf",
             },
             {
                 "id": 5.2,
                 "name": "Pesquisa, atención y diabetes en el adulto",
                 "type": "category",
+                "fecha": "09/2025",
                 "children": [
                      {
                         "id": 5.21,
                         "name": "Pesquisa, Diagnóstico y Tratamiento",
                         "type": "flowchart",
-                        "miniature1": "/static/images/5-b-a.png",
+                        "fecha": "09/2025",
                         "pdfDR": "/static/pdfs/5-B-A.pdf",
                     },
                     {
                         "id": 5.22,
                         "name": "Seguimiento y Complicaciones",
                         "type": "flowchart",
-                        "miniature1": "/static/images/5-b-b.png",
+                        "fecha": "09/2025",
                         "pdfDR": "/static/pdfs/5-B-B.pdf",
                     },
                 ]
@@ -152,7 +148,7 @@ OBJETIVOS_DATA = [
                 "id": 5.3,
                 "name": "Diagnóstico, tratamiento y seguimiento de EPOC",
                 "type": "flowchart",
-                "miniature1": "/static/images/5-c.png",
+                "fecha": "01/2026",
                 "pdfDR": "/static/pdfs/5-C.pdf",
             },
         ],
@@ -161,11 +157,13 @@ OBJETIVOS_DATA = [
         "id": 6,
         "name": "Prevención oportuna del cáncer en adulto",
         "type": "objective",
+        "fecha": "09/2025",
         "children": [
             {
-                "id": 6.11,
+                "id": 6.1,
                 "name": "Cáncer colorrectal",
                 "type": "flowchart",
+                "fecha": "09/2025",
                 "miniature1": "/static/images/6-a.png",
                 "pdfDR": "/static/pdfs/6-A.pdf",
             },
@@ -173,6 +171,7 @@ OBJETIVOS_DATA = [
                 "id": 6.2,
                 "name": "Cáncer cervicouterino",
                 "type": "flowchart",
+                "fecha": "06/2025",
                 "miniature1": "/static/images/6-b.png",
                 "pdfDR": "/static/pdfs/6-B.pdf",
             },
@@ -180,6 +179,7 @@ OBJETIVOS_DATA = [
                 "id": 6.3,
                 "name": "Cáncer de mama",
                 "type": "flowchart",
+                "fecha": "06/2025",
                 "miniature1": "/static/images/6-c.png",
                 "pdfDR": "/static/pdfs/6-C.pdf",
             },
@@ -189,17 +189,19 @@ OBJETIVOS_DATA = [
         "id": 7,
         "name": "Salud mental",
         "type": "objective",
+        "fecha": "01/2026",
         "children": [
             {
                 "id": 7.1,
                 "name": "Primer nivel de atención",
                 "type": "category",
+                "fecha": "09/2025",
                 "children": [
                     {
                         "id": 7.11,
-                        "name": "Diagnóstico, tratamiento y seguimiento de intento de suicidio",
+                        "name": "Diagnóstico, tratamiento y seguimiento de intento de suicidio y consumo problemático",
                         "type": "flowchart",
-                        "miniature1": "/static/images/7-a-a.png",
+                        "fecha": "09/2025",
                         "pdfDR": "/static/pdfs/7-A-A.pdf",
                     }
                 ]
@@ -208,27 +210,28 @@ OBJETIVOS_DATA = [
                 "id": 7.2,
                 "name": "Hospital Enrique Vera Barros",
                 "type": "category",
+                "fecha": "01/2026",
                 "children": [
                     {
                         "id": 7.21,
                         "name": "Servicio de salud mental",
                         "type": "flowchart",
-                        "miniature1": "/static/images/7-b-a.png",
+                        "fecha": "06/2025",
                         "pdfDR": "/static/pdfs/7-B-A.pdf",
                     },
                     {
                         "id": 7.22,
                         "name": "Servicio de adolescencia",
                         "type": "flowchart",
-                        "miniature1": "/static/images/7-b-b.png",
+                        "fecha": "06/2025",
                         "pdfDR": "/static/pdfs/7-B-B.pdf",
                     },
                     {
                         "id": 7.23,
                         "name": "Servicio de psicología",
                         "type": "flowchart",
-                        "miniature1": None,
-                        "pdfDR": None,
+                        "fecha": "01/2026",
+                        "pdfDR": "/static/pdfs/7-B-C.pdf", 
                     },
                 ]
             },
@@ -236,12 +239,13 @@ OBJETIVOS_DATA = [
                 "id": 7.3,
                 "name": "Hospital de la madre y el niño",
                 "type": "category",
+                "fecha": "06/2025",
                 "children": [
                     {
                         "id": 7.31,
                         "name": "Servicio de salud mental",
                         "type": "flowchart",
-                        "miniature1": "/static/images/7-c-a.png",
+                        "fecha": "06/2025",
                         "pdfDR": "/static/pdfs/7-C-A.pdf",
                     },
                 ]
@@ -252,72 +256,110 @@ OBJETIVOS_DATA = [
         "id": 8,
         "name": "Prevención y atención de enfermedades transmisibles",
         "type": "objective",
+        "fecha": "09/2025",
         "children": [
             {
                 "id": 8.1,
                 "name": "HIV",
                 "type": "flowchart",
-                "miniature1": "/static/images/8-a.png",
+                "fecha": "09/2025",
                 "pdfDR": "/static/pdfs/8-A.pdf",
             },
             {
-                "id": 8.21,
+                "id": 8.2,
                 "name": "Sífilis",
                 "type": "flowchart",
-                "miniature1": "/static/images/8-b.png",
+                "fecha": "09/2025",
                 "pdfDR": "/static/pdfs/8-B.pdf",
             },
             {
-                "id": 8.31,
+                "id": 8.3,
                 "name": "Tuberculosis",
                 "type": "flowchart",
-                "miniature1": "/static/images/8-c.png",
+                "fecha": "06/2025",
                 "pdfDR": "/static/pdfs/8-C.pdf",
             }
         ],
     },
+    {
+        "id": 9,
+        "name": "Garantía de Derechos de Accesibilidad en Salud",
+        "type": "objective",
+        "fecha": None,
+        "children": [
             {
-                "id": 9,
+                "id": 9.1,
+                "name": "Persona con Discapacidad",
+                "type": "flowchart",
+                "fecha": None,
+                "pdfDR": None,
+            },
+            {
+                "id": 9.2,
+                "name": "Violencia de Género",
+                "type": "flowchart",
+                "fecha": None,
+                "pdfDR": None,
+            }
+        ],
+    },
+            {
+                "id": 10,
                 "name": "Atención odontológica",
                 "type": "flowchart",
                 "category": "transversal",
-                "miniature1": "/static/images/ODON-TRANSVERSAL.png",
+                "fecha": "09/2025",
                 "pdfDR": "/static/pdfs/ODON-TRANSVERSAL.pdf",
             },
             {
-                "id": 10,
+                "id": 11,
                 "name": "Actividad física",
                 "type": "flowchart",
                 "category": "transversal",
-                "miniature1": "/static/images/ACT_FISICA_TRANSVERSAL..png",
+                "fecha": "06/2025",
                 "pdfDR": "/static/pdfs/ACT_FISICA_TRANSVERSAL.pdf",
             },
             {
-                "id": 11,
+                "id": 12,
                 "name": "Atención nutricional",
                 "type": "flowchart",
-                "category": "transversal",
-                "miniature1": "/static/images/NUTRI-TRANSVERSAL-SOBREPESO.png",
-                "pdfDR": "/static/pdfs/NUTRI-TRANSVERSAL-SOBREPESO.pdf",
-            },
-            {
-                "id": 12,
-                "name": "Enfermería",
-                "type": "objective",
+                "fecha": "06/2025",
                 "category": "transversal",
                 "children": [
                     {
                         "id": 12.1,
-                        "name": "Primer y Segundo Nivel de Atención",
+                        "name": "Sobrepeso y obesidad",
                         "type": "flowchart",
-                        "miniature1": "/static/images/ENF_TRANSVERSAL_N1_N2.png",
-                        "pdfDR": "/static/pdfs/ENF_TRANSVERSAL_N1_N2.pdf",
+                        "fecha": "06/2025",
+                        "pdfDR": "/static/pdfs/NUTRI-TRANSVERSAL-SOBREPESO.pdf",                    
                     },
                     {
                         "id": 12.2,
+                        "name": "Celiaquía",
+                        "type": "flowchart",
+                        "pdfDR":None,
+                    }
+                ], 
+            },
+            {
+                "id": 13,
+                "name": "Enfermería",
+                "type": "objective",
+                "fecha": "11/2025",
+                "category": "transversal",
+                "children": [
+                    {
+                        "id": 13.1,
+                        "name": "Primer y Segundo Nivel de Atención",
+                        "type": "flowchart",
+                        "fecha": "11/2025",
+                        "pdfDR": "/static/pdfs/ENF_TRANSVERSAL_N1_N2.pdf",
+                    },
+                    {
+                        "id": 13.2,
                         "name": "Tercer Nivel de Atención",
                         "type": "flowchart",
-                        "miniature1": "/static/images/ENF_TRANSVERSAL_N3.png",
+                        "fecha": "11/2025",
                         "pdfDR": "/static/pdfs/ENF_TRANSVERSAL_N3.pdf",
                     }
 
@@ -341,15 +383,28 @@ def validar_sesion(token: str) -> bool:
 async def dashboard(request: Request, t: Optional[str] = Query(None, description="Token opcional")):
     
     sesion_activa = validar_sesion(t)
+    objetivos_principales = [item for item in OBJETIVOS_DATA if item['id'] < 10]
+    rutas_transversales = [item for item in OBJETIVOS_DATA if item['id'] >= 10]
+
+    # --- NUEVA LÓGICA DE FECHAS ---
+    ahora = datetime.now()
     
-    objetivos_principales = [item for item in OBJETIVOS_DATA if item['id'] < 9]
-    rutas_transversales = [item for item in OBJETIVOS_DATA if item['id'] >= 9]
+    def es_reciente(fecha_str):
+        if not fecha_str:
+            return False
+        try:
+            fecha_item = datetime.strptime(fecha_str, "%m/%Y")
+            diferencia = ahora - fecha_item
+            return 0 <= diferencia.days <= 45
+        except:
+            return False
 
     return templates.TemplateResponse("dashboard.html", {
         "request": request,
         "sesion_activa": sesion_activa,
         "objetivos": objetivos_principales,
-        "transversales": rutas_transversales
+        "transversales": rutas_transversales,
+        "es_reciente": es_reciente  
     })
 
 @app.get("/")
